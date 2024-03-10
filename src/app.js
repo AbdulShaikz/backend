da95250eabbb02c66896aa0b17a53dfaea8a10e8
Abdul Shaik <starabdul100@gmail.com>
2024-03-10 17:53:15 +0530

    Add custom ApiError and ApiResponse classes, asyncHandling wrapper function in the utils

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}));
+
+
+app.use(express.json({
+    limit: "16kb",
+}));    //if data comes from form
+
+app.use(express.urlencoded({
+    extended: true,
+    limit: "16kb", 
+}));
+
+app.use(express.static("public"));  //storing files/assets on server which can be accessible
+app.use(cookieParser());    //cookieParser helps in accessing and setting cookies of the user browsers from the server
+
+export {app};
